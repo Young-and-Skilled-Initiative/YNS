9f27b202a6a03a8e5dd985c5202208d1b2acb2e0
ebubechi-ihediwa <ebubechiihediwa@gmail.com>
2024-09-06 06:28:59 +0100

    Built FAQ Section

---
+++ app/components/faq/faq-list.js
@@ -0,0 +1,22 @@
+export const faq_list = [
+  {
+    question: "What is Young & Skilled?",
+    answer: " Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    question: "What courses do you offer?",
+    answer: " Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    question: "How do I enroll in a course?",
+    answer: " Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    question: "Do I receive a certificate after completing a course?",
+    answer: " Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    question: "Can I access the courses on my mobile device?",
+    answer: " Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import FAQ from "@/app/components/faq/faq";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>yound and skilled</h1>
+      <FAQ />
     </main>
   );
 }
