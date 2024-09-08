e8c6c15c179aff4adf4b039d3775f5de7b851247
David <ejeredavid2001@gmail.com>
2024-09-08 06:22:17 +0100

    made finishing touches to the network section

--- app/components/network.jsx
+++ app/components/network.jsx
@@ -30,28 +30,24 @@ const Network = () => {
           <div className="z-30">
             <Image src={Lady} alt="Man image" className="" />
           </div>
-          <div className="flex z-30 bg-white rounded-full h-[3.25em] py-3 pl-1 pr-5 gap-3 items-center justify-center -ml-[7em] mt-[3em] w-[10.5em]">
-            <span className="bg-green-900 text-white rounded-full py-2 px-4 flex items-center">
-              J
+          <div className="flex z-30 bg-white rounded-full h-[3.25em] py-3 px-2 pr-5 gap-3 items-center  justify-center -ml-[7em] mt-[3em] ">
+            <span className="bg-green-900 text-white rounded-full  h-[2.5em] w-[2.5em] flex text-center items-center justify-center">
+              G
             </span>
             <span className="text-center justify-center items-center flex ">
-              James Ade
+              Gale Hillman
             </span>
           </div>
-          {/* <div className="z-10">
-            <Image src={Line} className=" mt-[4.5em] -ml-[6.5em]" />
-          </div> */}
         </div>
 
         {/* Lady Image (moved up with negative margin) */}
-        <div className="flex  items-end -mt-[3em] ml-[5em] relative ">
-
-          <div className="flex bg-black rounded-full h-[3.25em]  py-3 pl-2 -mr-[7em] pr-5 gap-3  z-30 items-center justify-center  mb-[8em]">
-            <span className="bg-orange-700 text-white rounded-full py-2 px-4 flex items-center">
-              G
+        <div className="flex  items-end -mt-[3em] ml-[8em] relative ">
+          <div className="flex bg-black rounded-full h-[3.25em]  py-3 px-2 -mr-[7em] pr-5 gap-3  z-30 items-center justify-center  mb-[8em]">
+            <span className="bg-orange-700 text-white rounded-full h-[2.5em] w-[2.5em] flex items-center justify-center">
+              J
             </span>
             <span className="text-center text-white justify-center items-center flex ">
-              Gale Hillman
+              James Ade
             </span>
           </div>
           <div className="flex flex-col z-20  mb-7">
