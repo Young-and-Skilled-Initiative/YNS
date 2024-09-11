87485cdc98834258f92ee63fdeb46b496e5e8041
David <ejeredavid2001@gmail.com>
2024-09-11 13:11:55 +0100

    fixed conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ import Hero from "./components/hero/hero";
 
 export default function Home() {
   return (
-    <>
+    < >
       <Navbar />
 
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
         <Hero/>
 
       <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24">
