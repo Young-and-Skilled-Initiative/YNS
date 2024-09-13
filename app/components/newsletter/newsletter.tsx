0be007d8dc031b8ab1a0998143a91b47ed9e40d3
David <ejeredavid2001@gmail.com>
2024-09-13 11:25:49 +0100

    made some changes to the hero section

--- app/components/newsletter/newsletter.tsx
+++ app/components/newsletter/newsletter.tsx
@@ -6,7 +6,7 @@ import "./newsletter.css"
 
 const newsletter = () => {
   return (
-    <div className="newsletter flex flex-col    rounded-3xl p-[2em] justify-between ">
+    <div className="newsletter flex flex-col gap-[18em]     rounded-3xl p-[2em] justify-between ">
       <div className=" flex flex-col ">
         <h1 className="font-cocon text-white text-[50px]">Join the Movement</h1>
         <p className="font-manrope text-white ">Are you ready to take the next step in your learning journey? Sign up  </p>
