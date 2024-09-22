e424f85657bf553b73533bef6394a00ae442c58c
Nwanne-san <113237530+Nwanne-san@users.noreply.github.com>
2024-09-22 22:49:25 +0100

    feat: configured navbar and footer with dropdown, hover transitions, and responsiveness

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,128 +1,132 @@
-
 import Link from "next/link";
 import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="p-[3em] text-white">
-      <div className="lg:px-[45px] px-6 py-[94px] bg-dark-green w-full rounded-[20px]">
+    <div className="sm:p-[3em] text-white">
+      <div className="lg:px-[45px] px-6 py-12 sm:py-[94px] bg-dark-green w-full rounded-[20px]">
         <div className="flex flex-col gap-[125px] w-full">
-          <div className="flex justify-between w-full h-full">
-            <div className="flex flex-col justify-between">
-              <img src="images/Logo.svg" alt="Logo" />
-              <img src="images/logo-sticker.svg" alt="logo sticker" />
-            </div>
-            <div className="flex flex-col gap-[30px] text-lg xl:text-lg lg:text-xl">
-              <span className="text-lighter-yellow">Company</span>
-              <ul className="flex flex-col gap-2">
-                <li>
-                  <a href="">About us</a>
-                </li>
-                <li>
-                  <a href="">Contact</a>
-                </li>
-                <li>
-                  <a href="">Partners</a>
-                </li>
-                <li>
-                  <a href="">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="">Cookies Policy</a>
-                </li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-[30px] text-lg xl:text-xl">
-              <span className="text-lighter-yellow">Resources</span>
-              <ul className="flex flex-col gap-2">
-                <li>
-                  <a href="">Blog</a>
-                </li>
-                <li>
-                  <a href="">Help Center</a>
-                </li>
-                <li>
-                  <a href="">Partners</a>
-                </li>
-                <li>
-                  <a href="">Courses</a>
-                </li>
-                <li>
-                  <a href="">Learn </a>
-                </li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-[30px] text-lg xl:text-xl">
-              <span className="text-lighter-yellow">Legal</span>
-              <ul className="flex flex-col gap-2">
-                <li>
-                  <a href="">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="">Terms of Use</a>
-                </li>
-                <li>
-                  <a href="">Cookie Policy</a>
-                </li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-[30px] text-lg xl:text-xl">
-              <span className="text-lighter-yellow">Socials</span>
-              <div className="flex flex-col gap-2.5">
-                <Link
-                  href={""}
-                  className="p-[13px] bg-white rounded-full flex w-fit"
-                >
-                  <Image
-                    src="/instagram-filled.svg"
-                    alt=""
-                    width={24}
-                    height={24}
-                    className=""
-                  />
-                </Link>
-                <Link
-                  href={""}
-                  className="p-[13px] bg-white rounded-full w-fit"
-                >
-                  <Image
-                    src="/facebook-filled.svg"
-                    alt=""
-                    width={24}
-                    height={24}
-                    className=""
-                  />
-                </Link>
-                <Link
-                  href={""}
-                  className="p-[13px] bg-white rounded-full w-fit"
-                >
-                  <Image
-                    src="/linkedin-filled.svg"
-                    alt=""
-                    width={24}
-                    height={24}
-                    className=""
-                  />
-                </Link>
-                <Link
-                  href={""}
-                  className="p-[13px] bg-white rounded-full w-fit flex items-center justify-center"
-                >
-                  <Image
-                    src="/x-filled.svg"
-                    alt=""
-                    width={24}
-                    height={24}
-                    className=""
-                  />
-                </Link>
+          <div className="sm:flex justify-between w-full h-full items-start ">
+            <img
+              src="images/Logo.svg"
+              alt="Logo"
+              className="flex justify-start items-start pb-[30px] sm:p-0"
+            />
+            {/* <img src="images/logo-sticker.svg" alt="logo sticker" /> */}
+
+            <div className="sm:flex grid grid-cols-2 sm:text-lg sm:gap-0 gap-x-14 gap-y-7 text-base  flex-row w-full justify-between">
+              <div className="flex flex-col gap-[30px]  xl:text-lg lg:text-xl">
+                <span className="text-lighter-yellow">Company</span>
+                <ul className="flex flex-col gap-2">
+                  <li>
+                    <a href="">About us</a>
+                  </li>
+                  <li>
+                    <a href="">Contact</a>
+                  </li>
+                  <li>
+                    <a href="">Partners</a>
+                  </li>
+                  <li>
+                    <a href="">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="">Cookies Policy</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-[30px]  xl:text-xl">
+                <span className="text-lighter-yellow">Resources</span>
+                <ul className="flex flex-col gap-2">
+                  <li>
+                    <a href="">Blog</a>
+                  </li>
+                  <li>
+                    <a href="">Help Center</a>
+                  </li>
+                  <li>
+                    <a href="">Partners</a>
+                  </li>
+                  <li>
+                    <a href="">Courses</a>
+                  </li>
+                  <li>
+                    <a href="">Learn </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-[30px] xl:text-xl">
+                <span className="text-lighter-yellow">Legal</span>
+                <ul className="flex flex-col gap-2">
+                  <li>
+                    <a href="">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="">Terms of Use</a>
+                  </li>
+                  <li>
+                    <a href="">Cookie Policy</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-[30px] text-lg xl:text-xl">
+                <span className="text-lighter-yellow">Socials</span>
+                <div className="sm:flex flex-col grid grid-cols-2 sm:gap-2.5 w-fit gap-y-2 gap-x-4">
+                  <Link
+                    href={""}
+                    className="p-[13px] bg-white rounded-full flex w-fit"
+                  >
+                    <Image
+                      src="/x-filled.svg"
+                      alt=""
+                      width={24}
+                      height={24}
+                      className=""
+                    />
+                  </Link>
+                  <Link
+                    href={""}
+                    className="p-[13px] bg-white rounded-full w-fit"
+                  >
+                    <Image
+                      src="/facebook-filled.svg"
+                      alt=""
+                      width={24}
+                      height={24}
+                      className=""
+                    />
+                  </Link>
+                  <Link
+                    href={""}
+                    className="p-[13px] bg-white rounded-full w-fit"
+                  >
+                    <Image
+                      src="/linkedin-filled.svg"
+                      alt=""
+                      width={24}
+                      height={24}
+                      className=""
+                    />
+                  </Link>
+                  <Link
+                    href={""}
+                    className="p-[13px] bg-white rounded-full w-fit flex items-center justify-center"
+                  >
+                    <Image
+                      src="/instagram-filled.svg"
+                      alt=""
+                      width={24}
+                      height={24}
+                      className=""
+                    />
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
           <div className="flex flex-col gap-[84px] items-center">
             <img src="/images/watermark.svg" alt="Watermark" />
-            <p className="text-xl font-normal text-gray-400">
+            <p className="text-base sm:text-xl font-normal text-gray-400">
               Copyright © 2024 Young & Skilled Initiative
             </p>
           </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,81 +1,192 @@
 "use client";
 
-import React from "react";
-import { usePathname } from "next/navigation";
+import React, { useState, useCallback, useRef } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+// Define your nav links
 const navlinks = [
-  {
-    route: "Home",
-    link: "",
-    id: "home",
-  },
-  {
-    route: "About us",
-    link: "/",
-    id: "about",
-  },
-  {
-    route: "Resources",
-    link: "/",
-    id: "resources",
-    icon: "images/arrow-up.svg",
-  },
-  {
-    route: "Contact us",
-    link: "/",
-    id: "contact-us",
-  },
-  {
-    route: "Blog",
-    link: "/",
-    id: "Blog",
-  },
+  { route: "Home", link: "", id: "home" },
+  { route: "About us", link: "/about", id: "about" },
+  { route: "Resources", link: "/resources", id: "resources" },
+  { route: "Contact us", link: "/contact", id: "contact-us" },
+  { route: "Blog", link: "/blog", id: "blog" },
 ];
 
-interface NavbarProps {
-  className?: string; // Explicitly typing the className prop as string
-}
-const Navbar: React.FC<NavbarProps> = ({ className }) => {
-  //   const pathname = usePathname();
-  //   console.log(pathname);
-  //   const currentPath = pathname?.split("/")[2];
-  //   console.log(currentPath);
+const Navbar: React.FC = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const underlineRef = useRef(null);
+  const [hoverStyle, setHoverStyle] = useState({ width: 0, left: 0 });
+
+  const handleMouseEnter = (e) => {
+    const { offsetLeft, offsetWidth } = e.currentTarget;
+    setHoverStyle({ width: offsetWidth, left: offsetLeft });
+  };
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  const closeMobileMenu = () => {
+    setShowMobileMenu(false);
+  };
+
   return (
     <>
-      <div className="w-full bg-white xl:px-28 px-4 flex justify-between gap-6 py-12">
-        <img src="images/logo-main.svg" alt="" />
-        <div className="flex items-center justify-between gap-2 lg:gap-10 text-black">
+      <div className="w-full bg-white xl:px-28 px-4 py-6 flex justify-between gap-6 lg:py-12">
+        <img
+          src="/images/logo-main.svg"
+          alt="Logo"
+          className="w-[60px] h-[33px] sm:w-auto sm:h-auto"
+        />
+
+        {/* Desktop menu */}
+        <div className="relative hidden md:flex items-center justify-between gap-2 lg:gap-10 text-black">
+          {/* Shared underline element */}
+          <span
+            ref={underlineRef}
+            className="absolute bottom-0 h-[3px] bg-dark-green transition-all duration-300"
+            style={hoverStyle}
+          ></span>
+
           {navlinks.map((item, index) => (
             <Link
               key={index}
               href={item.link}
-              className={`text-base font-semibold transition-all p-[10px] text-nowrap duration-200 text-ash hover:text-black
-                 `}
+              className="relative group text-base font-semibold transition-all p-[6px] text-nowrap duration-200 text-ash hover:text-black"
+              onMouseEnter={handleMouseEnter}
             >
-              <span className="flex gap-2">
-                {item.route}
-                {item.icon && item.route === "Resources" && (
-                  <img src={item.icon} alt="" />
-                )}
-              </span>
+              {item.route}
             </Link>
           ))}
         </div>
-        <div className="flex items-center">
-          <Button size={"lg"} className="rounded-3xl bg-dark-green">
+
+        {/* Action buttons */}
+        <div className="hidden md:flex items-center group">
+          <Button
+            size={"lg"}
+            className="rounded-3xl bg-dark-green transform transition-all duration-300 ease-in-out group-hover:scale-100 group-hover:px- group-hover:text-base hover:bg-dark-green"
+          >
             Get Started
           </Button>
           <Button
             size={"icon"}
-            className="rounded-full bg-dark-green ml-[-6px] "
+            className="rounded-full bg-dark-green ml-[-6px] transform transition-transform duration-300 ease-in-out group-hover:rotate-90"
           >
-            <img src="/arrow.svg" alt="" className="p-2" />
+            <img src="/arrow.svg" alt="Arrow" className="p-2" />
           </Button>
         </div>
+
+        {/* Hamburger for mobile */}
+        <button
+          onClick={toggleMobileMenu}
+          className="md:hidden focus:outline-none"
+        >
+          <motion.img
+            src={showMobileMenu ? "/hamburger-close.svg" : "/hamburger.svg"}
+            alt="Menu"
+            className="w-8 h-8"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          />
+        </button>
       </div>
+
+      {/* Mobile Menu */}
+      <AnimatePresence>
+        {showMobileMenu && (
+          <motion.div
+            className="fixed top-0 left-0 w-full h-[80%] bg-black z-50 flex flex-col items-center justify-between px-4 py-6"
+            initial={{ y: "-100%", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: "-100%", opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            onClick={closeMobileMenu} // Close on outside click
+          >
+            {/* Logo inside the dropdown */}
+            <div className="flex w-full justify-between">
+              <img
+                src="/images/logo.svg"
+                alt="Logo"
+                className="w-[60px] h-[33px]"
+              />
+              {/* Close button inside dropdown */}
+              <button onClick={closeMobileMenu} className="">
+                <motion.img
+                  src="/hamburger-close.svg"
+                  alt="Close"
+                  className="w-8 h-8"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.2 }}
+                />
+              </button>
+            </div>
+
+            <nav className="flex flex-col h-full justify-center items-center gap-12 w-full">
+              {navlinks.map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.link}
+                  className="text-white font-semibold text-lg hover:text-gray-400"
+                  onClick={closeMobileMenu} // Close menu on link click
+                >
+                  {item.route}
+                </Link>
+              ))}
+              <Button
+                size={"lg"}
+                className="rounded-[50px] border-2 border-white !py-2.5 !text-base bg-dark-green w-full"
+              >
+                Get Started
+              </Button>
+              <div className="flex justify-end w-full gap-[75px]">
+                <div className="flex gap-4 items-center ">
+                  <Link href={``}>
+                    <Image
+                      src={`x-outlined.svg`}
+                      alt="X-logo"
+                      width={20}
+                      height={20}
+                    />
+                  </Link>
+                  <Link href={``}>
+                    <Image
+                      src={`Facebook-outlined.svg`}
+                      alt="X-logo"
+                      width={20}
+                      height={20}
+                    />
+                  </Link>
+                  <Link href={``}>
+                    <Image
+                      src={`Linkedin-outlined.svg`}
+                      alt="X-logo"
+                      width={20}
+                      height={20}
+                    />
+                  </Link>
+                </div>
+                <button className="w-[4x] h-[47px">
+                  <img
+                    src="Messenger.svg"
+                    alt="intercom messenger"
+                    className=""
+                  />
+                </button>
+              </div>
+            </nav>
+            <p className="text-xs font-normal text-gray-400">
+              Copyright © 2024 Young & Skilled Initiative all rights reserved
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
