987a3f6b67504ca401956f30940e6edffdf5da3c
Mereson <chimeremnmaojinta@gmail.com>
2024-09-26 07:08:52 +0100

    responsive design for ysi story

--- app/aboutus/components/TheYsiStory.tsx
+++ app/aboutus/components/TheYsiStory.tsx
@@ -10,9 +10,9 @@ import React from "react"
 
 const TheYsiStory = () => {
   return (
-    <section className="bg-[#1A1A1A] pt-[4.8rem] pb-[5.5rem] px-[9rem] flex gap-[4.656rem] items-center justify-center ">
+    <section className="bg-[#1A1A1A] pt-[4.8rem] pb-[5.5rem] px-[4rem] lg:px-[9rem] flex lg:gap-[3rem] xl:gap-[4.656rem] flex-wrap lg:flex-nowrap items-center justify-center ">
       <section>
-        <figure className="relative h-[520px] w-[410px]">
+        <figure className="relative h-[452px] lg:h-[520px] w-[360px] lg:w-[410px]">
           <div className="absolute z-10">
             <Founders pic={YsiPic1} name={"Fada Usk"} />
           </div>
@@ -23,15 +23,19 @@ const TheYsiStory = () => {
             <Image
               src={YsiTapStar}
               alt="tap star"
-              className="absolute z-20 left-[-4.5rem] bottom-[-4rem]"
+              className="absolute z-20 left-[-2rem] bottom-[-3rem] lg:left-[-3.5rem] lg:bottom-[-3.5rem] xl:left-[-4rem] xl:bottom-[-4rem] w-28 lg:w-[146px] xl:w-[159.34px]"
             />
           </div>
         </figure>
       </section>
-      <section className="">
-        <article className="relative w-[35.5rem]">
-          <Image src={YsiStar} alt="star image" className="absolute right-0" />
-          <h1 className="text-[#EF4C0D] text-[50px] font-medium font-[Cocon]">
+      <section className="mt-20 lg:mt-0">
+        <article className="relative lg:w-[30.5rem] xl:w-[35.5rem]">
+          <Image
+            src={YsiStar}
+            alt="star image"
+            className="absolute right-[-50px] w-16 lg:w-[80px]"
+          />
+          <h1 className="text-[#EF4C0D] text-[35px] md:text-[50px] font-medium font-[Cocon]">
             The YSI story
           </h1>
           <div className="text-[#F6DEAE] pt-[27px]">
@@ -69,13 +73,15 @@ interface FoundersProps {
 
 const Founders: React.FC<FoundersProps> = ({ pic, name, tilt }) => {
   return (
-    <div className="relative h-[524px] w-[400px]">
+    <div className="relative h-[500px] lg:h-[524px] w-[350px] lg:w-[400px]">
       <Image src={pic} alt="Founders Image" />
       <div
-        className={`absolute bottom-6 left-[18px] ${tilt} w-[364px] h-[79px] bg-[#F2A300] flex gap-5 rounded-[10px] items-center px-[17px]`}
+        className={`absolute bottom-16 lg:bottom-6 left-[16px] lg:left-[18px] ${tilt} w-[320px] lg:w-[364px] h-[62px] lg:h-[79px] bg-[#F2A300] flex gap-3 lg:gap-5 rounded-[10px] items-center px-[17px]`}
       >
-        <p className="font-[Cocon] font-medium text-[40px]">{name}</p>
-        <p className="w-[144px] text-white text-xl leading-[21px]">
+        <p className="font-[Cocon] font-medium text-[32px] lg:text-[40px]">
+          {name}
+        </p>
+        <p className="w-[124px] lg:w-[144px] text-white text-lg lg:text-xl leading-[21px]">
           Co-Founder <span className="font-bold"> YS initiative</span>
         </p>
       </div>
