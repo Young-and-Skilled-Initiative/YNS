c67bd232f082cc68be5188cbbeb9cf149d7d556e
David <ejeredavid2001@gmail.com>
2024-09-26 12:30:48 +0100

    added the nav and footer

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -3,15 +3,19 @@ import TeamMembers from "./components/TeamMembers"
 import WhoIsIntrested from "./components/WhoIsIntrested"
 import TheYsiStory from "./components/TheYsiStory"
 import Interest from "./components/interest"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const page: React.FC = () => {
   return (
     <>
+    <Navbar/>
       <TheYsiStory />
       <Interest/>
       <div className="px-5 md:px-9 lg:px-[2.5rem]">
         <WhoIsIntrested />
         <TeamMembers />
+        <Footer/>
       </div>
     </>
   )
