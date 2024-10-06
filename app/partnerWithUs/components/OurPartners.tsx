bd8336ade73325e2fe73ba1fe7eb471142da176f
Mereson <chimeremnmaojinta@gmail.com>
2024-10-06 14:47:32 +0100

    partner with us page 90% done

--- app/partnerWithUs/components/OurPartners.tsx
+++ app/partnerWithUs/components/OurPartners.tsx
@@ -10,54 +10,29 @@ import {
   Kuda,
   Stripe,
 } from "../../../public/images/partnerWithUs"
-import useEmblaCarousel from "embla-carousel-react"
-import Autoplay from "embla-carousel-autoplay" // Import the correct autoplay plugin
-import { EmblaOptionsType } from "embla-carousel"
 
-type PropType = {
-  options?: EmblaOptionsType
-}
-
-const OurPartners: React.FC<PropType> = ({ options }) => {
-  const autoplayOptions = Autoplay({
-    delay: 3000, // 3 seconds delay
-    stopOnInteraction: false, // Keep autoplay running even after user interaction
-    stopOnMouseEnter: true, // Stops when the user hovers over the carousel
-  })
-
-  const [emblaRef] = useEmblaCarousel(options, [autoplayOptions]) // Pass the autoplay plugin to the Embla carousel
-
+const OurPartners: React.FC = () => {
   return (
     <section className="">
-      <h1 className="ml-[106px] text-[40px] text-[#2F2F2F] font-lato leading-[44px]">
+      <h1 className="ml-5 xl:ml-[106px] text-[30px] xl:text-[40px] text-[#2F2F2F] font-lato leading-[44px]">
         Our Partners
       </h1>
 
-      <div className="embla pt-[40px]">
-        <div className="embla__viewport" ref={emblaRef}>
-          <div className="embla__container flex gap-[80px]">
-            <div className="embla__slide">
-              <Image src={Corywise} alt="Corywise Logo" />
-            </div>
-            <div className="embla__slide">
-              <Image src={Microsoft} alt="Microsoft Logo" />
-            </div>
-            <div className="embla__slide">
-              <Image src={Autocheck} alt="Autocheck Logo" />
-            </div>
-            <div className="embla__slide">
-              <Image src={Flutterwave} alt="Flutterwave Logo" />
-            </div>
-            <div className="embla__slide">
-              <Image src={EvolveCredit} alt="EvolveCredit Logo" />
-            </div>
-            <div className="embla__slide">
-              <Image src={Kuda} alt="Kuda Logo" />
-            </div>
-            <div className="embla__slide">
-              <Image src={Stripe} alt="Stripe Logo" />
-            </div>
-          </div>
+      <div className="overflow-hidden pt-[40px]">
+        <div className="flex flex-wrap xl:flex-nowrap pl-4 lg:pl-0 gap-[80px]">
+          <Image src={Corywise} alt="Corywise Logo" />
+
+          <Image src={Microsoft} alt="Microsoft Logo" />
+
+          <Image src={Autocheck} alt="Autocheck Logo" />
+
+          <Image src={Flutterwave} alt="Flutterwave Logo" />
+
+          <Image src={EvolveCredit} alt="EvolveCredit Logo" />
+
+          <Image src={Kuda} alt="Kuda Logo" />
+
+          <Image src={Stripe} alt="Stripe Logo" />
         </div>
       </div>
     </section>
--- app/partnerWithUs/components/WhyPartnerWithUS.tsx
+++ app/partnerWithUs/components/WhyPartnerWithUS.tsx
@@ -7,13 +7,17 @@ import {
 
 const WhyPartnerWithUS: React.FC = () => {
   return (
-    <section className="pt-[120px]">
-      <h2 className="text-[40px] font-lato text-[#2F2F2F]">
+    <section className="pt-[120px] grid place-items-center">
+      <h2 className="text-[30px] xl:text-[40px] justify-start w-full font-lato text-[#2F2F2F]">
         Why partner with us?
       </h2>
-      <section className="pt-[72px] grid gap-[120px]">
-        <div className="flex gap-[23px] items-center">
-          <Image src={BlogHeadshot1} width={403} height={314} alt="Image One" />
+      <section className="pt-[40px] w-[335px] md:w-full lg:pt-[72px] grid gap-12 xl:gap-[120px]">
+        <div className="flex flex-wrap gap-[23px] items-center">
+          <Image
+            src={BlogHeadshot1}
+            className="w-[335px] lg:w-[403px] h-[314px]"
+            alt="Image One"
+          />
           <Article
             info1="Visibility"
             info2="Brand Awareness"
@@ -21,17 +25,25 @@ const WhyPartnerWithUS: React.FC = () => {
             text="Partnering with us gives your brand the opportunity to make a lasting impact in the education sector. Align your organization with a meaningful cause that resonates with socially responsible and forward-thinking audiences"
           />
         </div>
-        <div className="flex gap-[23px] justify-end items-center">
+        <div className="flex flex-wrap-reverse gap-[23px] md:justify-end items-center">
           <Article
             info1="Talent Pipeline"
             info2="Innovation"
             title="Access a Network of Future Innovators"
             text="By collaborating with Young and Skilled Initiative, you'll gain direct access to a community of talented, skilled young individuals who are poised to become tomorrow's industry leaders and innovators."
           />
-          <Image src={BlogHeadshot2} width={403} height={314} alt="Image One" />
+          <Image
+            src={BlogHeadshot2}
+            className="w-[335px] lg:w-[403px] h-[314px]"
+            alt="Image One"
+          />
         </div>
-        <div className="flex gap-[23px] items-center">
-          <Image src={BlogHeadshot3} width={403} height={314} alt="Image One" />
+        <div className="flex flex-wrap gap-[23px] items-center">
+          <Image
+            src={BlogHeadshot3}
+            className="w-[335px] lg:w-[403px] h-[314px]"
+            alt="Image One"
+          />
           <Article
             info1="Sustainability"
             info2="Reputation"
@@ -55,13 +67,15 @@ const Article: React.FC<ArticleProps> = ({ info1, info2, title, text }) => {
   const float: string =
     "px-4 py-3 rounded-[44.98px] border-[1px] border-[#8E9BAE] text-[#8E9BAE] text-sm leading-4"
   return (
-    <article className="grid gap-[14px] w-[394px] font-lato">
+    <article className="grid gap-[14px] w-[335px] lg:w-[394px] font-lato">
       <div className="flex gap-4">
         <p className={float}>{info1}</p>
         <p className={float}>{info2}</p>
       </div>
-      <h2 className="text-[#1A1A1A] text-2xl ">{title}</h2>
-      <p className="leading-[32px] text-[#8E9BAE]">{text}</p>
+      <h2 className="text-[#1A1A1A] text-base xl:text-2xl ">{title}</h2>
+      <p className="leading-[32px] text-sm xl:text-base text-[#8E9BAE]">
+        {text}
+      </p>
     </article>
   )
 }
