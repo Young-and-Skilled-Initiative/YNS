49e503875864819004516a73e19bdb477823ec7f
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-10-08 12:42:58 +0100

    refactor: improved the blog page

--- app/blog/components/BlogFeatured.tsx
+++ app/blog/components/BlogFeatured.tsx
@@ -1,5 +1,5 @@
 import { blogCards } from "@/data";
-import { ArrowLeft } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import React from "react";
 import Slider from "react-slick";
 
@@ -14,47 +14,51 @@ const BlogFeatured = () => {
   };
 
   return (
-  <div className="slider-container text-black w-full flex flex-col justify-center">  <Slider {...settings}>
-  {blogCards.map((card, index) => (
-    <div key={index}>
-      <div className="flex">
-        <div className="relative w-1/2">
-          {" "}
-          <img
-            src={card.image}
-            alt={card.title}
-            className="w-24 h-24 object-cover rounded-3xl"
-          />
-          <p className="absolute top-0 left-0 px-4 py-2 rounded-tl-3xl">Featured</p>
-        </div>
-        <div className="px-14 py-5 flex flex-col gap-4 text-left w-1/2">
-          <div className="flex gap-5">
-            {card.category.map((cat, idx) => (
-              <span
-                key={idx}
-                className="text-base font-medium text-gray-300 px-2 py-1 "
-              >
-                {cat}
-              </span>
-            ))}
+    <div className="slider-container text-black w-full flex flex-col justify-center my-24 font-manrope">
+      {" "}
+      <Slider {...settings}>
+        {blogCards.map((card, index) => (
+          <div key={index} className="pb-6">
+            <div className="flex">
+              <div className="relative w-1/2">
+                {" "}
+                <img
+                  src={card.image}
+                  alt={card.title}
+                  className="w-full h-full object-cover rounded-[1.25rem]"
+                />
+                <p className="absolute top-0 left-0 px-4 py-2 rounded-tl-[1.25rem] bg-dark-green text-white">
+                  Featured
+                </p>
+              </div>
+              <div className="px-14 py-5 flex flex-col gap-4 text-left w-1/2">
+                <div className="flex gap-5">
+                  {card.category.map((cat, idx) => (
+                    <span
+                      key={idx}
+                      className="text-base font-medium text-gray-400  "
+                    >
+                      {cat}
+                    </span>
+                  ))}
+                </div>
+                <h3 className="text-3xl font-medium font-cocon leading-9 ">
+                  {card.title}
+                </h3>
+                <p className=" text-gray-600 text-base leading-8 ">
+                  {card.description}
+                </p>
+                <p className="text-gray-400 text-base ">{card.date}</p>
+
+                <button className="flex gap-3 ">
+                  Read Article <ArrowRight />
+                </button>
+              </div>
+            </div>
           </div>
-          <h3 className="text-3xl font-medium font-cocon">{card.title}</h3>
-          <p className=" text-gray-600 text-base  font-manrope">
-            {card.description}
-          </p>
-          <p className="text-gray-500 text-base font-manrope">
-            {card.date}
-          </p>
-          <p>
-            <button>
-              Read Article <ArrowLeft />
-            </button>
-          </p>
-        </div>
-      </div>
+        ))}
+      </Slider>
     </div>
-  ))}
-</Slider></div>
   );
 };
 
