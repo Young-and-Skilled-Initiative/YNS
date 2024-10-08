00232eeaae598aee97ffad5310a1c1229d4a87c9
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-10-08 09:48:40 +0100

    refactor: improved the blog page

--- app/blog/components/NewsAndInsightsBlogHero.tsx
+++ app/blog/components/NewsAndInsightsBlogHero.tsx
@@ -1,10 +1,12 @@
+"use client"
 import { Star } from "@/public/images";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const NewsAndInsightsBlogHero = () => {
   const categories = ["All", "Education", "Career", "Company", "Courses"];
   const backgroundImage = "/images/blog-hero.png"; // Update this with your image path
+  const [activeCategory, setActiveCategory] = useState(0);
   return (
     <section
       style={{
@@ -18,7 +20,7 @@ const NewsAndInsightsBlogHero = () => {
         {/* Title */}
         <div className="flex flex-col gap-4 ">
           <div className="relative flex gap-14 w-fit ">
-            <h1 className="text-7xl  font-medium ">
+            <h1 className="text-7xl  font-medium font-cocon">
               News Insights & <span className="bg-nine-color-blocks text-transparent bg-clip-text">Blog</span>
             </h1>
 
@@ -28,7 +30,7 @@ const NewsAndInsightsBlogHero = () => {
                 width={100}
                 height={100}
                 alt="star"
-                className="absolute bottom-6"
+                className="absolute bottom-6 hover:animate-spin "
               />
             </div>
           </div>
@@ -42,11 +44,12 @@ const NewsAndInsightsBlogHero = () => {
 
         {/* Category Buttons */}
         <div className="flex  space-x-5">
-          {categories.map((category, index) => (
+        {categories.map((category, index) => (
             <button
               key={index}
+              onClick={() => setActiveCategory(index)} // Set the clicked category as active
               className={`${
-                index === 0 ? "bg-white text-black" : "bg-white/30 text-white"
+                activeCategory === index ? "bg-white text-black" : "bg-white/30 text-white"
               } py-4 backdrop-blur-4 px-10 rounded-[2.75rem] hover:bg-white hover:text-black transition-all duration-500 ease-in-out`}
             >
               {category}
