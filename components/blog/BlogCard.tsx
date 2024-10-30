64c95e758a5cceabd269abb0d2076a1325ab3c9f
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-10-30 23:39:47 +0100

    refactor: updated the blogs post

--- components/blog/BlogCard.tsx
+++ components/blog/BlogCard.tsx
@@ -1,30 +1,37 @@
 import { BlogCardData } from "@/types";
+import Link from "next/link";
 import React from "react";
+// Slugify utility function
+const generateSlug = (title: string) => {
+  return title.toLowerCase().replace(/[^a-z0-9]+/g, "-");
+};
 
 const BlogCard: React.FC<BlogCardData> = ({ category, title, date, image }) => {
   return (
-    <div className="flex flex-col  rounded-lg gap-3 justify-between">
-      <div className="flex flex-col  gap-3">
-        <div className="flex justify-center mb-3">
-          <img src={image} alt={title} className="w-full h-full" />
+    <Link href={`/blog/${generateSlug(title)}`}>
+      <div className="flex flex-col  rounded-lg gap-3 justify-between">
+        <div className="flex flex-col  gap-3">
+          <div className="flex justify-center mb-3">
+            <img src={image} alt={title} className="w-full h-full" />
+          </div>
+          <div className="text-sm text-gray-500 space-x-2">
+            {category.map((cat, i) => (
+              <span
+                className="font-medium text-base font-manrope text-gray-400"
+                key={i}
+              >
+                {cat}
+              </span>
+            ))}
+          </div>
+          <h3 className="text-2xl font-medium font-cocon">{title}</h3>
         </div>
-        <div className="text-sm text-gray-500 space-x-2">
-          {category.map((cat, i) => (
-            <span
-              className="font-medium text-base font-manrope text-gray-400"
-              key={i}
-            >
-              {cat}
-            </span>
-          ))}
+        <div className="flex flex-col  gap-3">
+          {" "}
+          <p className="text-gray-400 font-manrope font-medium">{date}</p>
         </div>
-        <h3 className="text-2xl font-medium font-cocon">{title}</h3>
       </div>
-      <div className="flex flex-col  gap-3">
-        {" "}
-        <p className="text-gray-400 font-manrope font-medium">{date}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
