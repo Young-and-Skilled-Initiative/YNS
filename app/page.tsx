19e0bcdb1563fdd88c828c893b8898417d012282
David <ejeredavid2001@gmail.com>
2024-11-04 17:42:35 +0100

    made changes to the page file

--- app/page.tsx
+++ app/page.tsx
@@ -29,8 +29,7 @@ export default function Home() {
         <Testimonial />
         <Newsletter />
 
-        <Testimonial />
-        <Newsletter />
+ 
 
         <NewsInsightsAndBlog />
       </main>
