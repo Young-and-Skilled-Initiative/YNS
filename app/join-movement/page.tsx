6b7e2c30ab483b5dac3eca95a13a06bf3cbbe98c
ebubechi-ihediwa <ebubechiihediwa@gmail.com>
2024-12-03 23:31:15 +0100

    Resolved issue with join-movement

--- app/join-movement/page.tsx
+++ app/join-movement/page.tsx
@@ -6,14 +6,14 @@ import NewsInsightsAndBlog from "../components/NewsInsightsAndBlog";
 import Faq from "../components/Faq/faq";
 import Footer from "../components/Footer";
 
-export const JoinMovement = () => {
+const JoinMovement = () => {
   return (
     <div className="w-full">
       <Navbar />
       <div className="flex flex-col justify-center items-center gap-20 sm:gap-[100px] sm:px-[6.25em] px-5">
-        <MovementHero/>
+        <MovementHero />
         <NewsInsightsAndBlog />
-        <Faq/>
+        <Faq />
       </div>
       <div className="px-5 sm:px-[3.25em]">
         <Footer />
